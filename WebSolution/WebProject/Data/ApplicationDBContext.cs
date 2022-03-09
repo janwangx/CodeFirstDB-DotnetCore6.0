95b91082cf2f6a131a85dfe512069fadf30c590b
Janaki Wanigasooriya <43464654+janakiw@users.noreply.github.com>
2022-03-09 11:27:15 +0530

    initialCommit
    s
    cls

---
+++ WebSolution/WebProject/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProject.Models;
+
+namespace WebProject.Data
+{
+    public class ApplicationDBContext :DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> tblCategory  { get; set; }
+    }
+}
